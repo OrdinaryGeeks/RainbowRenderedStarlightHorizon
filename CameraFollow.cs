46fe5d2ebb2bd8ef3542e1986abf915ee4ffdfa4
Natty <alecto_perfecto@hotmail.com>
2019-12-06 12:40:12 -0600

    Initial commit for SSRH

---
+++ CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject starlight;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = starlight.transform.position + new Vector3(0.0f, 24.0f, 0.0f);
+    }
+}
